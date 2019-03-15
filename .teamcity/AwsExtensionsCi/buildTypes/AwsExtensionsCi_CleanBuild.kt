2ea95174f6c57a56027700f7ac30b80caa6521ae
akshays <akshays@TeamCity>
2019-03-15 10:12:57 +0000

    TeamCity change in 'AWS_Extensions_CI' project: 1 build step(s) were added to 'Clean Build' build configuration

--- .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_CleanBuild.kt
+++ .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_CleanBuild.kt
@@ -1,6 +1,7 @@
 package AwsExtensionsCi.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 
 object AwsExtensionsCi_CleanBuild : BuildType({
     uuid = "61af96cf-8f4a-498e-ac76-a0ed419d1054"
@@ -8,5 +9,14 @@ object AwsExtensionsCi_CleanBuild : BuildType({
 
     vcs {
         root(AwsExtensionsCi.vcsRoots.AwsExtensionsCi_HttpsGithubComAkshayAppdAwsEc2monitoringExtensionCiRefsHeadsMast)
+    }
+
+    steps {
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.JDK_18%"
+        }
     }
 })
