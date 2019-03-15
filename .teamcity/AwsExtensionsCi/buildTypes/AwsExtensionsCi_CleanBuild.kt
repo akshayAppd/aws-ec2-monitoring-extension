e8292b38e3b909f0515542eb1198691ef22a42e8
akshays <akshays@TeamCity>
2019-03-15 10:12:24 +0000

    TeamCity change in 'AWS_Extensions_CI' project: 'Clean Build' build configuration was created

--- .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_CleanBuild.kt
+++ .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_CleanBuild.kt
@@ -1,40 +1,8 @@
 package AwsExtensionsCi.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
-import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 object AwsExtensionsCi_CleanBuild : BuildType({
-    uuid = "b593b361-3cc1-45dc-ad92-ee0f6454fef8"
+    uuid = "61af96cf-8f4a-498e-ac76-a0ed419d1054"
     name = "Clean Build"
-
-    vcs {
-        root(AwsExtensionsCi.vcsRoots.AwsExtensionsCi_HttpsGithubComAkshayAppdAwsEc2monitoringExtensionCiRefsHeadsMast)
-    }
-
-    steps {
-        dockerCompose {
-            file = "docker-compose.yml"
-        }
-        maven {
-            goals = "clean test"
-            runnerArgs = "-Dmaven.test.failure.ignore=true"
-            mavenVersion = defaultProvidedVersion()
-            jdkHome = "%env.JDK_18%"
-        }
-        dockerCommand {
-            commandType = build {
-                source = path {
-                    path = "Dockerfile"
-                }
-            }
-        }
-    }
-
-    triggers {
-        vcs {
-        }
-    }
 })
