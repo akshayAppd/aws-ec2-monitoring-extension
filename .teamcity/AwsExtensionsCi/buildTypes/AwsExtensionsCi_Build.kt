5cc58341c22d1996a0c4fb036651d29e8ce0c296
akshays <akshays@TeamCity>
2019-03-15 10:01:57 +0000

    TeamCity change in 'AWS_Extensions_CI' project: 3 build step(s) were added to 'Build' build configuration

--- .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_Build.kt
+++ .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_Build.kt
@@ -1,6 +1,9 @@
 package AwsExtensionsCi.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
+import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.maven
 import jetbrains.buildServer.configs.kotlin.v2018_2.triggers.vcs
 
 object AwsExtensionsCi_Build : BuildType({
@@ -9,6 +12,25 @@ object AwsExtensionsCi_Build : BuildType({
 
     vcs {
         root(AwsExtensionsCi.vcsRoots.AwsExtensionsCi_HttpsGithubComAkshayAppdAwsEc2monitoringExtensionCiRefsHeadsMast)
+    }
+
+    steps {
+        dockerCommand {
+            commandType = build {
+                source = path {
+                    path = "Dockerfile"
+                }
+            }
+        }
+        dockerCompose {
+            file = "docker-compose.yml"
+        }
+        maven {
+            goals = "clean test"
+            runnerArgs = "-Dmaven.test.failure.ignore=true"
+            mavenVersion = defaultProvidedVersion()
+            jdkHome = "%env.JDK_18%"
+        }
     }
 
     triggers {
