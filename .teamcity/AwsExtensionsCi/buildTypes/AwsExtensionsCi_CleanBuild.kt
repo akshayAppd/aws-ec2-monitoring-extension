eb00e58c431888a8309830cc8a75bc482d4aeed3
akshays <akshays@TeamCity>
2019-03-15 10:12:43 +0000

    TeamCity change in 'AWS_Extensions_CI' project: VCS roots of 'Clean Build' build configuration were updated

--- .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_CleanBuild.kt
+++ .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_CleanBuild.kt
@@ -5,4 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.*
 object AwsExtensionsCi_CleanBuild : BuildType({
     uuid = "61af96cf-8f4a-498e-ac76-a0ed419d1054"
     name = "Clean Build"
+
+    vcs {
+        root(AwsExtensionsCi.vcsRoots.AwsExtensionsCi_HttpsGithubComAkshayAppdAwsEc2monitoringExtensionCiRefsHeadsMast)
+    }
 })
