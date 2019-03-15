761dbba565edc04ee9f6bb86f3f51fbaefe98072
akshays <akshays@TeamCity>
2019-03-15 10:17:15 +0000

    TeamCity change in 'AWS_Extensions_CI' project: VCS roots of 'Setup Linux Environment' build configuration were updated

--- .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_SetupLinuxEnvironment.kt
+++ .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_SetupLinuxEnvironment.kt
@@ -5,4 +5,8 @@ import jetbrains.buildServer.configs.kotlin.v2018_2.*
 object AwsExtensionsCi_SetupLinuxEnvironment : BuildType({
     uuid = "be445f3f-f3de-4caa-9921-050f4360c464"
     name = "Setup Linux Environment"
+
+    vcs {
+        root(AwsExtensionsCi.vcsRoots.AwsExtensionsCi_HttpsGithubComAkshayAppdAwsEc2monitoringExtensionCiRefsHeadsMast)
+    }
 })
