64d59fc80600ef46575a560197fa7621561487eb
akshays <akshays@TeamCity>
2019-03-15 10:18:43 +0000

    TeamCity change in 'AWS_Extensions_CI' project: runners of 'Setup Linux Environment' build configuration were updated

--- .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_SetupLinuxEnvironment.kt
+++ .teamcity/AwsExtensionsCi/buildTypes/AwsExtensionsCi_SetupLinuxEnvironment.kt
@@ -2,7 +2,6 @@ package AwsExtensionsCi.buildTypes
 
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCommand
-import jetbrains.buildServer.configs.kotlin.v2018_2.buildSteps.dockerCompose
 
 object AwsExtensionsCi_SetupLinuxEnvironment : BuildType({
     uuid = "be445f3f-f3de-4caa-9921-050f4360c464"
@@ -13,9 +12,6 @@ object AwsExtensionsCi_SetupLinuxEnvironment : BuildType({
     }
 
     steps {
-        dockerCompose {
-            file = "docker-compose.yml"
-        }
         dockerCommand {
             commandType = build {
                 source = path {
