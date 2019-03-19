1239cb36a356c73d430adc861e13b34627332401
akshayAppd <akshay.srivastava@appdynamics.com>
2019-03-19 13:19:30 +0530

    AWS EC2 btd check in

--- .teamcity/BtdAws_Ec2MonitoringExtension/vcsRoots/BtdAws_Ec2MonitoringExtension_AkshayGit.kt
+++ .teamcity/BtdAws_Ec2MonitoringExtension/vcsRoots/BtdAws_Ec2MonitoringExtension_AkshayGit.kt
@@ -4,6 +4,7 @@ import jetbrains.buildServer.configs.kotlin.v2017_2.vcs.GitVcsRoot
 
 object BtdAws_Ec2MonitoringExtension_AkshayGit : GitVcsRoot({
     uuid = "15440bfc-4a14-11e9-8646-d663bd873d93"
+    id = "BtdAws_Ec2MonitoringExtension_AkshayGit"
     name = "AkshayGit"
     url = "git@github.com:akshayAppd/aws-ec2-monitoring-extension-ci.git"
     authMethod = uploadedKey {
