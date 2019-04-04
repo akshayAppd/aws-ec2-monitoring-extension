ed2125f1f76b916e154cb4eedea57e22b87f2cef
akshayAppd <akshay.srivastava@appdynamics.com>
2019-04-04 15:27:40 +0530

    AWS EC2 btd check in

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
 	region = "us-west-2"
-	access_key="${env.AWS_ACCESS_KEY.value}"
-	secret_key="${env.AWS_SECRET_KEY.value}"
+	#access_key="${env.AWS_ACCESS_KEY.value}"
+	#secret_key="${env.AWS_SECRET_KEY.value}"
 }
 
 resource "aws_instance" "aws_btd" {
