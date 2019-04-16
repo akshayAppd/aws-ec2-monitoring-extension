682e6bec841bac0e029a01ab3c5ecd93126ae522
akshayAppd <akshay.srivastava@appdynamics.com>
2019-04-16 12:57:12 +0530

    AWS EC2 btd check in

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@
 provider "aws" {
   #access_key = "${var.aws_access_key}"
   #secret_key = "${var.aws_secret_key}"
-  region     = "us-east-1"
+  region     = "us-west-2"
 }
 
 resource "aws_instance" "aws_btd" {
-  ami = "ami-032509850cf9ee54e"
+  ami = "ami-061392db613a6357b"
   instance_type = "t2.micro"
 
   #Reading the subnet_id from variables.tf
