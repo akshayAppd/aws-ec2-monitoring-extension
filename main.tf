eae4615e5fdf20882cd8ea2e3e8db547da0bfb89
akshayAppd <akshay.srivastava@appdynamics.com>
2019-04-20 01:04:01 +0530

    AWS EC2 btd check in

--- main.tf
+++ main.tf
@@ -4,12 +4,13 @@
 provider "aws" {
   #access_key = "${var.aws_access_key}"
   #secret_key = "${var.aws_secret_key}"
-  region     = "us-west-2"
+  region     = "eu-central-1"
 }
 
 resource "aws_instance" "aws_btd" {
   ami = "ami-061392db613a6357b"
   instance_type = "t2.micro"
+  iam_instance_profile = "arn:aws:ec2:eu-central-1::instance/*"
 
   #Reading the subnet_id from variables.tf
   subnet_id = "subnet-3c3bbb75"
