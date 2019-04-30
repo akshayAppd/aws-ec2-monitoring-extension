674543e65bdf3023638c62e37ec2a94da36945d3
akshayAppd <akshay.srivastava@appdynamics.com>
2019-04-30 15:46:32 +0530

    AWS EC2 btd check in

--- main.tf
+++ main.tf
@@ -5,17 +5,20 @@ provider "aws" {
   #access_key = "${var.aws_access_key}"
   #secret_key = "${var.aws_secret_key}"
   region     = "eu-central-1"
+  #region = "us-west-2"
+  shared_credentials_file = "creds"
+  profile                 = "default"
 }
 
 resource "aws_instance" "aws_btd" {
-  ami = "ami-09def150731bdbcc2"
+  ami = "ami-061392db613a6357b"
   instance_type = "t2.micro"
-  iam_instance_profile = "extensions-teamcity-artifact-storage-role"
-  availability_zone = "eu-central-1a"
+  #iam_instance_profile = "extensions-teamcity-artifact-storage-role"
+  availability_zone = "us-west-2b"
 
   #Reading the subnet_id from variables.tf
   #subnet_id = "subnet-3c3bbb75"
-  
+
   #Security group
-  #security_groups = ["sg-0a15a568fbcd99f41"]
+  #security_groups = ["sg-ccb3ebb"]
 }
