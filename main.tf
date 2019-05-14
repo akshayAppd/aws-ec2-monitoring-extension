adef09c18777c9c8f800fff90f8a7373894b29a7
akshayAppd <akshay.srivastava@appdynamics.com>
2019-05-14 16:50:55 +0530

    AWS EC2 btd check in

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   region     = "eu-central-1"
   shared_credentials_file = "creds"
-  profile                 = "appdext"
+  profile                 = "appdcs"
 }
 
 resource "aws_instance" "aws_btd" {
