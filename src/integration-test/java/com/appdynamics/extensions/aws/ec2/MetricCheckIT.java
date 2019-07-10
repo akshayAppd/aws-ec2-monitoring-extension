f665ec78282dc6b5127eafb9c42178f6061c069f
akshayAppd <akshay.srivastava@appdynamics.com>
2019-07-10 15:08:02 +0530

    AWS EC2 btd changed aws commons version

--- src/integration-test/java/com/appdynamics/extensions/aws/ec2/MetricCheckIT.java
+++ src/integration-test/java/com/appdynamics/extensions/aws/ec2/MetricCheckIT.java
@@ -79,7 +79,7 @@ public class MetricCheckIT {
 
         UrlBuilder builder = UrlBuilder.builder();
         builder.host(CONTROLLER_HOST).port(CONTROLLER_PORT).ssl(Boolean.valueOf(CONTROLLER_SSL_ENABLED)).path("controller/rest/applications/Server%20&%20Infrastructure%20Monitoring/metric-data");
-        builder.query("metric-path", "Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CAmazon%20EC2%7CAWS%7CAPI%7CCalls");
+        builder.query("metric-path", "Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CAmazon%20EC2%7CAWS%20API%20Calls");
         builder.query("time-range-type", "BEFORE_NOW");
         builder.query("duration-in-mins", "1");
         builder.query("output", "JSON");
@@ -122,7 +122,7 @@ public class MetricCheckIT {
 
         UrlBuilder builder = UrlBuilder.builder();
         builder.host(CONTROLLER_HOST).port(CONTROLLER_PORT).ssl(Boolean.valueOf(CONTROLLER_SSL_ENABLED)).path("controller/rest/applications/Server%20&%20Infrastructure%20Monitoring/metric-data");
-        builder.query("metric-path", "Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CAmazon%20EC2%7CAppD%7Cus-west-2%7CInstance%7Cbtd_ec2%7CCPUUtilization");
+        builder.query("metric-path", "Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CAmazon%20EC2%7CAppD%7Cus-west-2%7CInstance%7Cbtd-ec2%7CCPUUtilization");
         builder.query("time-range-type", "BEFORE_NOW");
         builder.query("duration-in-mins", "1");
         builder.query("output", "JSON");
